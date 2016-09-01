626e311847886fc98289a7c0680c2ccf122d648b
Seila Gonzalez <seila.gonzalez@matrix.msu.edu>
2016-09-01 15:51:04 -0400

    fullrecord changes

--- fullrecord.php
+++ fullrecord.php
@@ -14,6 +14,7 @@ define('WP_USE_THEMES', true);
 	$restful_url=$_POST['restful'];
 	$query = "KID,=,".$k;
 	//$fields = 'ALL';
+	
 
 	if(isset($_POST['fields'])){
 		$fields = $_POST['fields'];
@@ -29,100 +30,61 @@ define('WP_USE_THEMES', true);
 	///capture results and display
 	$server_output = curl_exec($ch);
 	
-	 $detail=str_replace("koraobj_container","container_full",$server_output);
-	 $replace=str_replace("koraobj_control","control_full",$detail);
-	 $replace1=str_replace("koraobj_control_label","control_label_full",$replace);
-	 $replace2=str_replace("koraobj_control_value","control_value_full",$replace1);
-     echo "<a id='backArrow' href='javascript:history.go(-1)'><img src='images/Arrow-Left.svg'/></a>";
-	//echo $replace2;
+    echo "<a id='backArrow' href='javascript:history.go(-1)'><img src='images/Arrow-Left.svg'/></a>";
 	
 	$server_output = json_decode($server_output, true);
-	
-	//var_dump($restful_url);
-	
-	//var_dump($k);
-	
-	
-	foreach ($server_output as $kid => $koraobj)
-			{
-				$htmlout .= "<div class='koraobj_container'>\n";
-				// OUTPUT THE KID ALSO
-				$htmlout .= "\t<div class='koraobj_control koraobj_control_kid' ><div class='koraobj_control_label'>KID</div>";
-				$htmlout .= "<div class='koraobj_control_value'>".$kid."</div></div>\n";
-				
-				$newArr;
-				
-				foreach ($koraobj as $dfield => $dvalue)
-				{	
-			
-					if (is_array($dvalue)) {
-						$arStr = '';
-						foreach( $dvalue as $key => $value ) {
-							if ($value != '') {
-								$arStr .= $value."\n";
-							}							
-						}
-						
-						$newArr [] = $arStr;
-					} else {
-						$newArr [] = $dvalue;
-					}
-					if (!(isset($dvalue))){
-						//var_dump($dvalue);
-						//var_dump("empty");
-					}
-					
-						// SKIP THIS ROW UNLESS SHOWEMPTY HAS BEEN SET TO TRUE
-						//if ( $koraobj == null ||  $koraobj == '') { continue; } 
-/*
-						//$ctlDisplay = new $controls[$dfield]['type']($pid,$controls[$dfield]['cid'],$kid);
-						if(is_a($ctlDisplay,'ImageControl')){
-							$htmlout .= "\t<div class='koraobj_control koraobj_control_".htmlentities($controls[$dfield]['name'],ENT_QUOTES)."' ><div class='koraobj_control_label'>".$controls[$dfield]['name']."</div>";
-							$imagecontrolstring = $ctlDisplay->showData();
-							$imagepos = strrpos($imagecontrolstring,"<div class");
-							$htmlout .= "<div class='koraobj_control_value'>" . 
-							     substr($imagecontrolstring,0, $imagepos) . 
-							     "</div></div>\n";
-							$htmlpictureout .= "<div class='koraobj_control_value'>" . 
-							     substr($imagecontrolstring,$imagepos) . 
-							     "</div>";
-						}else if(is_a($ctlDisplay,'FileControl')){
-							$htmlout .= "\t<div class='koraobj_control koraobj_control_".htmlentities($controls[$dfield]['name'],ENT_QUOTES)."' ><div class='koraobj_control_label'>".$controls[$dfield]['name']."</div>";
-							$htmlout .= "<div class='koraobj_control_value'>" . 
-							     $ctlDisplay->showData() . 
-								"</div></div>\n";
-							if(strpos($htmlout,'Audio:') !== false){
-								$audvidcontrolstring = $ctlDisplay->showData();
-								$part1 = explode('<audio src',$audvidcontrolstring)[1];
-								$part2 = explode('</audio>',$part1)[0];
-								$htmlpictureout .= "<div class='koraobj_control_value'><audio src" . 
-							     $part2 . 
-							     "</audio></div>";
-							}
-							if(strpos($htmlout,'Video:') !== false){
-								$audvidcontrolstring = $ctlDisplay->showData();
-								$part1 = explode('<video src',$audvidcontrolstring)[1];
-								$part2 = explode('</video>',$part1)[0];
-								$htmlpictureout .= "<div class='koraobj_control_value'><video src" . 
-							     $part2 . 
-							     "</video></div>";
-							}
-						}
-						else {
-							$htmlout .= "\t<div class='koraobj_control koraobj_control_".htmlentities($controls[$dfield]['name'],ENT_QUOTES)."' ><div class='koraobj_control_label'>".$controls[$dfield]['name']."</div>";
-							$htmlout .= "<div class='koraobj_control_value'>" . 
-							     $ctlDisplay->showData() . 
-								"</div></div>\n";
-*/
-						//}
 
-					//}
+
+		//preview of media if present 
+		foreach ($server_output as $kid => $koraobj) {	
+				
+			$decKID = explode("-", $k);
+			
+			if ($koraobj["Video File"] != '') {
+				$prevHTML = '<div class="control_full_value">';
+				$prevHTML .= '<div class="kc_file_tn">';
+				$prevHTML .= '<video controls><source src="' . kora_url . 'files/' . hexdec($decKID[0]) . '/' . hexdec($decKID[1]) . '/' .  $koraobj["Video File"]['localName'] . '"></video>';
+				$prevHTML .= '</div></div>';
+				echo($prevHTML);
+			} else if ($koraobj["Audio File"] != '') {
+				$prevHTML .= '<div class="control_full_value">';
+				$prevHTML .= '<div class="kc_file_tn">';
+				$prevHTML .= '<audio controls><source src="' . kora_url . 'files/' . hexdec($decKID[0]) . '/' . hexdec($decKID[1]) . '/' .  $koraobj["Audio File"]['localName'] . '"></audio>';
+				$prevHTML .= '</div></div>';
+				echo($prevHTML);
+			} else if ($koraobj["Image"] != '') {
+				$prevHTML = '<div class="control_full_value">';
+				$prevHTML .= '<div class="kc_file_tn">';
+				$prevHTML .= '<img src="' . kora_url . 'files/' . hexdec($decKID[0]) . '/' . hexdec($decKID[1]) . '/' .  $koraobj["Image"]['localName'] . '">';
+				$prevHTML .= '</div></div>';
+				echo($prevHTML);
+			}	
+		}
+		?>		
+	
+	<?php //metadata display  ?>
+	<div class="container_full">
+	<?php 
+	foreach ($server_output as $kid => $koraobj) {
+		$htmlout .= "<div class='koraobj_container'>\n";				
+		foreach ($koraobj as $dfield => $dvalue) {	
+			if (is_array($dvalue)) {
+				foreach( $dvalue as $key => $value ) {
+					if ($value != '') {
+						$htmlout .= "\t<div class='koraobj_control control_full koraobj_control_".$key."' ><div class='koraobj_control_label'>".$key."</div>";
+						$htmlout .= "<div class='koraobj_control_value'>" . $value.  "</div></div>\n";
+					}
 				}
-				
-				$htmlout .= "</div>\n";
-				//echo($htmlout);
-				
+			} else {
+				if ($dvalue != '') {
+					$htmlout .= "\t<div class='koraobj_control control_full koraobj_control_".$dfield."' ><div class='koraobj_control_label control_full_label'>".$dfield."</div>";
+					$htmlout .= "<div class='koraobj_control_value'>" . $dvalue.  "</div></div>\n";
+				}
 			}
-			var_dump($server_output);
-	var_dump($newArr);
+		}
+		$htmlout .= "</div>\n";
+		echo($htmlout);
+	}
 	?>
+	
+	</div>
